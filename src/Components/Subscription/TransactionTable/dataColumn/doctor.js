e4b63bba8cce134a86797771aff55b884c6f5ceb
akshay nagargoje <akshay.nagargoje@myrl.tech>
2022-10-28 12:00:22 +0530

    subscription changes

--- src/Components/Subscription/TransactionTable/dataColumn/doctor.js
+++ src/Components/Subscription/TransactionTable/dataColumn/doctor.js
@@ -2,17 +2,23 @@ import React from "react";
 import { TABLE_DEFAULT_BLANK_FIELD } from "../../../../constant";
 import { getName } from "../../../../Helper/validation";
 import { Tooltip, Avatar } from "antd";
+import isEmpty from "../../../../Helper/is-empty";
 
 export default (props) => {
   const { transactions } = props || {};
-  const { doctor: { full_name = "" } = {} } = transactions;
+  // const { doctor: { full_name = "" } = {} } = transactions || {};
+
+  let fullName = "";
+  if (!isEmpty(transactions) && !isEmpty(transactions.doctor)) {
+    fullName = transactions.doctor.full_name;
+  }
 
   return (
     <div className="ellipsis wp100 flex direction-column  ">
-      <div className="wp100 fs16 fw600">{full_name}</div>
+      <div className="wp100 fs16 fw600">{fullName}</div>
       <div className="wp100 ">
         {/* {feeType ? `${feeType}` : TABLE_DEFAULT_BLANK_FIELD} */}
-        akshay@gmail.com
+        {/* akshay@gmail.com */}
       </div>
     </div>
   );
