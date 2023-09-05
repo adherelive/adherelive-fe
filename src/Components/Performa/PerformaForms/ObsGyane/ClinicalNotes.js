afb177db13a2ae5ffd0227843f4bd6bfeffceed4
akshay nagargoje <akshay.nagargoje@myrl.tech>
2023-09-05 13:45:57 +0530

    latest chnages added

--- src/Components/Performa/PerformaForms/ObsGyane/ClinicalNotes.js
+++ src/Components/Performa/PerformaForms/ObsGyane/ClinicalNotes.js
@@ -1,24 +1,63 @@
 import React, { useState } from "react";
+import LabReportsOptions from "./Dropdowns/LabReportsOptions";
+import { PlusCircleOutlined, MinusCircleOutlined } from "@ant-design/icons";
+import isEmpty from "../../../../Helper/is-empty";
+import moment from "moment";
 
-function UsgFindings() {
-  const [inputFields, setInputFields] = useState([{ name: "", age: "" }]);
+function ClinicalNotes() {
+  const [inputFields, setInputFields] = useState([]);
+  const [isCollapse, setCollapse] = useState("");
 
-  const handleFormChange = (index, event) => {
+  const handleFormChange = (reportIndex, index, event) => {
     let data = [...inputFields];
-    data[index][event.target.name] = event.target.value;
+    data[reportIndex].reportValues[index][event.target.name] =
+      event.target.value;
     setInputFields(data);
   };
 
-  const addFields = () => {
-    let newfield = { name: "", age: "" };
-
+  const addFields = (reportName) => {
+    let allData = inputFields;
+    let filterData =
+      !isEmpty(allData) &&
+      allData.filter((ele) => ele.reportName === reportName);
+    // if (isEmpty(filterData)) {
+    let newfield = {
+      contentBlock: "Lab Report",
+      reportName: `${reportName}`,
+      reportValues: [
+        {
+          value: "",
+          date: new Date().toDateString(),
+          doctorName: "Akshay Nagargoje",
+        },
+      ],
+    };
+    setCollapse(reportName);
     setInputFields([...inputFields, newfield]);
+    // } else {
+    //   alert(`${reportName} is already added`);
+    // }
   };
 
-  const removeFields = (index) => {
+  const addValueHandler = (index, label) => {
+    setCollapse(label);
     let data = [...inputFields];
-    data.splice(index, 1);
+    data[index].reportValues.push({
+      value: "",
+      date: new Date().toDateString(),
+      doctorName: "Akshay Nagargoje",
+    });
     setInputFields(data);
+  };
+
+  const removeFields = (reportIndex, index) => {
+    let data = [...inputFields];
+    data[reportIndex].reportValues.splice(index, 1);
+    setInputFields(data);
+  };
+
+  const collapseHanlder = (label) => {
+    setCollapse(label);
   };
 
   const submit = (e) => {
@@ -27,33 +66,84 @@ function UsgFindings() {
   };
 
   return (
-    <div className="performa-form-container">
+    <div className="performa-lab-report-form-container">
       <form onSubmit={submit}>
         <h1>CLINICAL NOTES</h1>
-        <button onClick={addFields}>Add More..</button>
-        {inputFields.map((input, index) => {
+        <LabReportsOptions
+          addFields={addFields}
+          setInputFields={setInputFields}
+        />
+        {/* <button onClick={addFields}>Add More..</button> */}
+        {inputFields.map((report, reportIndex) => {
           return (
-            <div key={index}>
-              <input
-                name="name"
-                placeholder="Report name"
-                value={input.name}
-                onChange={(event) => handleFormChange(index, event)}
-              />
-              <input
-                name="age"
-                placeholder="value"
-                value={input.age}
-                onChange={(event) => handleFormChange(index, event)}
-              />
-              <button onClick={() => removeFields(index)}>Remove</button>
+            <div key={reportIndex}>
+              <div className="heading">
+                {isCollapse === report.reportName ? (
+                  <MinusCircleOutlined onClick={() => collapseHanlder("")} />
+                ) : (
+                  <PlusCircleOutlined
+                    onClick={() => collapseHanlder(report.reportName)}
+                  />
+                )}
+                <h3>{report.reportName}</h3>
+                <p
+                  onClick={() =>
+                    addValueHandler(reportIndex, report.reportName)
+                  }
+                  className="translate-text pointer mr10"
+                >
+                  Add Value
+                </p>
+              </div>
+              {isCollapse === report.reportName && (
+                <ul className="values-container">
+                  {!isEmpty(report.reportValues) ? (
+                    report.reportValues.map((input, index) => {
+                      return (
+                        <li style={{ marginBottom: "10px" }} key={index}>
+                          <input
+                            name="value"
+                            placeholder="value"
+                            value={input.value}
+                            onChange={(event) =>
+                              handleFormChange(reportIndex, index, event)
+                            }
+                          />
+
+                          <input
+                            name="value"
+                            placeholder="value"
+                            value={`Dr ${input.doctorName}`}
+                            disabled
+                          />
+
+                          <input
+                            name="value"
+                            placeholder="value"
+                            value={moment(input.date).format("YYYY-MM-DD")}
+                            disabled
+                          />
+
+                          <button
+                            onClick={() => removeFields(reportIndex, index)}
+                          >
+                            Remove
+                          </button>
+                        </li>
+                      );
+                    })
+                  ) : (
+                    <div>No Values added for {report.reportName}</div>
+                  )}
+                </ul>
+              )}
             </div>
           );
         })}
-        <button onClick={submit}>Submit</button>
+        {/* <button onClick={submit}>Submit</button> */}
       </form>
     </div>
   );
 }
 
-export default UsgFindings;
+export default ClinicalNotes;
--- src/Components/Performa/PerformaForms/ObsGyane/Dropdowns/UsgFindingsOption.js
+++ src/Components/Performa/PerformaForms/ObsGyane/Dropdowns/UsgFindingsOption.js
@@ -1,6 +1,6 @@
 import { Select, Space } from "antd";
 
-const LabReportsOptions = ({ addFields, setInputFields }) => {
+const UsgFindingsOption = ({ addFields, setInputFields }) => {
   const handleChange = (value) => {
     addFields(value);
     console.log(`selected ${value}`);
@@ -14,14 +14,13 @@ const LabReportsOptions = ({ addFields, setInputFields }) => {
         style={{ width: 120, marginBottom: "10px", marginLeft: "20px" }}
         onChange={handleChange}
         options={[
-          { value: "Hb", label: "Hb" },
-          { value: "TLC", label: "TLC" },
-          { value: "DLC", label: "DLC" },
-          { value: "LFT", label: "LFT" },
+          { value: "1st Trimester", label: "1st Trimester" },
+          { value: "2nd Trimester", label: "2nd Trimester" },
+          { value: "3rd Trimester", label: "3rd Trimester" },
         ]}
       />
     </div>
   );
 };
 
-export default LabReportsOptions;
+export default UsgFindingsOption;
