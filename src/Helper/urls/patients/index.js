43d20b8128df0349dc9fbfe6ca55b196ddc2b383
Gagneet Singh <gagneet@gmail.com>
2024-12-24 14:18:17 +1100

    API name changed to /api/prescription/details/ from detailss

--- src/Helper/urls/patients/index.js
+++ src/Helper/urls/patients/index.js
@@ -32,7 +32,7 @@ export const addCareplanForPatientUrl = (id) => {
 
 export const generatePrescriptionUrl = (careplan_id) => {
   // return `/api/patients/generate_prescription/${careplan_id}`;
-  return `/api/prescription/detailss/${careplan_id}`;
+  return `/api/prescription/details/${careplan_id}`;
 };
 
 export const getPatientsPaginatedUrl = (data) => {
