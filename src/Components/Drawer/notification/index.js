0876dc13d1c4bffa6ecfa9de5814493beac30beb
Gagneet Singh <gagneet@gmail.com>
2025-02-04 20:55:26 +1100

    Updated react-redux to v7.2.0, due to upStream function not found on v6.0.1

--- src/Components/Drawer/notification/index.js
+++ src/Components/Drawer/notification/index.js
@@ -536,7 +536,7 @@ class NotificationDrawer extends Component {
 
         for (let each in notifications) {
             const notification = notifications[each] || {};
-            console.log("get All Notification: ", {notification});
+            console.log("Get All Notification: ", {notification});
             const {
                 time: date = "",
                 is_read = false,
@@ -550,13 +550,14 @@ class NotificationDrawer extends Component {
                 eachDate = moment(start_time).format("Do MMM YYYY");
             }
 
-            // if (is_read) {
-            //   dataTorender && read.push(dataTorender);
-            // } else {
-            //   dataTorender && unread.push(dataTorender);
-            // }
-
-            // notificationComponents.push(dataTorender);
+            /**
+             * TODO: Why is this code not utilized?
+            if (is_read) {
+              dataTorender && read.push(dataTorender);
+            } else {
+              dataTorender && unread.push(dataTorender);
+            }
+            notificationComponents.push(dataTorender);*/
 
             const displayDate = (
                 <div className="ml8 mt10 mb10 fw800" key={`${eachDate}`}>
